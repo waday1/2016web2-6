b4b65a314f0c24170a7aac60381c95142a81308c
waday1 <g1527079@st.t-kougei.ac.jp>
2016-11-07 16:37:25 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 var http = require('http')
-http.createServer(function(req,res)｛
+http.createServer(function(req,res){
                   res.writeHead(200,{'Content-Type':'text/html'});
 var response_str =
     '<!DOCTYPE html>\n'+
 '<html>\n'+
-'<head>\n'+
-'<meta charset ="UTF-8">\n'+
-'<title>初めてのMicrosoft Azure</title>\n'+
-'</head>\n'+
-'<body>\n'+
-'<h1>Hallo Wo0rld</h1>\n'+
-'</body>\n'+
+'   <head>\n'+
+'     <meta charset ="UTF-8">\n'+
+'     <title>初めてのMicrosoft Azure</title>\n'+
+'   </head>\n'+
+'   <body>\n'+
+'     <h1>Hello World</h1>\n'+
+'   </body>\n'+
 '</html>\n';
 res.end(response_str);
 }).listen(process.env.PORT || 1337);
